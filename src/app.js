c02805a6ccbbd2e86ecf5a80a336b84c437e9a6f
Cristian <dcris3344@gmail.com>
2024-02-29 19:30:14 -0500

    Config CORS

--- src/app.js
+++ src/app.js
@@ -10,6 +10,10 @@ app.use(indexRoutes)
 app.use('/api', empleadoRoutes)
 
 app.use((req, res, next)=>{
+    res.header('Access-Control-Allow-Origin', '*'); // Permitir solicitudes desde cualquier origen
+    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Métodos permitidos
+    res.header('Access-Control-Allow-Headers', 'Content-Type'); // Encabezados permitidos
+    next();
     res.status(404).json({
         message: "endpoint not found"
     })
