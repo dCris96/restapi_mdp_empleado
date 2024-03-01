848c6fadf624bcfdcbc621a5c7d79f35a796601e
Cristian <dcris3344@gmail.com>
2024-02-29 21:13:15 -0500

    CORS

--- src/app.js
+++ src/app.js
@@ -1,16 +1,16 @@
 import express from 'express'
+import cors from 'cors'
 import empleadoRoutes from './routes/empleado.routes.js'
 import indexRoutes from './routes/index.routes.js'
-import cors from 'cors'
 
 const app = express()
 
 app.use(express.json())
 
+app.use(cors())
+
 app.use(indexRoutes)
 app.use('/api', empleadoRoutes)
-
-app.use(cors())
 
 app.use((req, res, next)=>{    
     res.status(404).json({
