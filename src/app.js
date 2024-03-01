c3240245018945eae6635ecc38f59487a266c0cf
Cristian <dcris3344@gmail.com>
2024-02-29 20:05:29 -0500

    Edit CORS

--- src/app.js
+++ src/app.js
@@ -13,10 +13,12 @@ app.use((req, res, next)=>{
     res.header('Access-Control-Allow-Origin', '*'); // Permitir solicitudes desde cualquier origen
     res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE'); // Métodos permitidos
     res.header('Access-Control-Allow-Headers', 'Content-Type'); // Encabezados permitidos
-    next();
+    
     res.status(404).json({
         message: "endpoint not found"
     })
+
+    next();
 })
 
 export default app
